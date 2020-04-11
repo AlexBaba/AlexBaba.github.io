929c146c74cf7062071525eb320d67d6b583e4cb
AlexBaba <djaliababoa@gmail.com>
2020-04-11 14:29:10 +0200

    Uploading webapp-page files htmls

---
+++ City Streetball webapp Norwegian Oslo Streetbasketball/script.js
@@ -0,0 +1,24 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    playlistId: 'PLlVlyGVtvuVkUpgWlBV44heQGeVpxFvU4',
+    events: {
+      'onReady': function() {
+        $(".video-thumb").click(function() {
+          var $this = $(this);
+          if (!$this.hasClass("active")) {
+            player.loadVideoById($this.attr("data-video"));
+                        $this.addClass("active").siblings().removeClass("active");
+          }
+        });
+      }
+    }
+  });
+}
