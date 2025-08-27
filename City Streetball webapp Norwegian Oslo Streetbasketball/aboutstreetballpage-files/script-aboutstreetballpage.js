8b1899d28f2fd603d52139a69de2ae932ff73e91
AlexBaba <djaliababoa@gmail.com>
2025-08-27 23:52:51 +0200

    SMall update - added a new Norwegian language version switcher, page: About Streetbasketball Page

---
+++ City Streetball webapp Norwegian Oslo Streetbasketball/aboutstreetballpage-files/script-aboutstreetballpage.js
@@ -0,0 +1,9 @@
+// script.js
+function changeLanguage(lang) {
+    if (lang === 'en') {
+        window.location.href = 'aboutstreetball.html';
+    } else if (lang === 'no') {
+        window.location.href = 'aboutstreetball-no.html';
+    }
+    // Add more conditions for other languages as needed
+}
